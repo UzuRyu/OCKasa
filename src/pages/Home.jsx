5ba1d902c297ac37b7f70bb1b748402286fc2555
UzuRyu <uzuryu@gmail.com>
2024-01-03 03:57:37 +0100

    CSS terminé à 99%

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,18 @@ import Banner from "../components/Banner";
 import BannerImg from "../Image source 1.png"
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import "./Home.scss"
 
 function Home() {
     return (
-    <>
-    <NavBar/ >
-    <Banner bannerTxt={"Chez vous, partout et ailleurs"} bannerImg={BannerImg} />
-    <Gallery />
-    <Footer />
-    </>
+        <>
+            <NavBar />
+            <div className="bannergallery_container">
+                <Banner bannerTxt={"Chez vous, partout et ailleurs"} bannerImg={BannerImg} />
+                <Gallery />
+            </div>
+            <Footer />
+        </>
     );
 }
 
